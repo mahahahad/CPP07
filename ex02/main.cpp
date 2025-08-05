785a32dddc05f05814cdc5ae2a0ef82566cce001
mahahahad <mahad.abd2003@gmail.com>
2025-08-05 13:47:55 +0400

    feat: update main to include more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,53 +1,50 @@
-#include <iostream>
-#include <Array.hpp>
+#include "Array.hpp"
 
-#define MAX_VAL 1024
-int main(int, char**)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+class   Complex {
+    public:
+        Complex() { val = 10; };
+        int     getVal() const { return (val); };
+        void    setVal(int newVal) { val = newVal; };
+    private:
+        int     val;
+};
 
-    for (int i = 0; i < MAX_VAL; i++)
+std::ostream&   operator<<(std::ostream& output, const Complex& val) {
+    output << val.getVal();
+    return (output);
+}
+
+int main() {
+    // {
+    //     // Deep copy testing
+    //     Array<int> tab(5);
+    //     const Array<int> tabCopy = tab;
+
+    //     std::cout << "Original int tab: "; tab.print();
+    //     std::cout << "Copied int tab: "; tabCopy.print();
+    //     tab[1] = 10;
+    //     std::cout << "Original int tab: "; tab.print();
+    //     std::cout << "Copied int tab: "; tabCopy.print();
+    // }
+
     {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
+        // Custom type testing
+        Array<Complex> tab(4);
+
+        tab.print();
+        tab[0].setVal(9);
+        tab.print();
+        for (unsigned int i = 0; i < tab.size(); i++) {
+            tab[i].setVal(tab[i].getVal() + i);
+        }
+        tab.print();
+        try {
+            std::cout << tab[10] << std::endl;;
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            std::cerr << "There are only " << tab.size() << " elements in your array. (Array is zero-indexed)" << std::endl;
         }
     }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+    
+    return (0);
 }
